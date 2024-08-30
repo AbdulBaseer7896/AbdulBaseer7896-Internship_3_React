c3462b3545a775a452917fc233a4aed7b69c3942
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2024-08-30 23:54:37 +0500

    just front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import ResultPage from './pages/ResultPage';
+import Home from './pages/Home';
+import Ranking from './pages/Ranking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+<Route path="/ResultPage"  element={<ResultPage />}></Route>
+<Route path="/Ranking"  element={<Ranking />}></Route>
+      </Routes>
+    </Router>
   );
 }
+
+
 
 export default App;
